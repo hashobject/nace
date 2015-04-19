8fac6450c716a4747098b4c0040218e946bfd84c
Anton Podviaznikov <podviaznikov@gmail.com>
2015-04-19 14:47:58 -0700

    init

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject nace "0.1.0-SNAPSHOT"
+  :description "NACE codes"
+  :signing {:gpg-key "Hashobject Ltd <team@hashobject.com>"}
+  :url "https://github.com/hashobject/nace"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
